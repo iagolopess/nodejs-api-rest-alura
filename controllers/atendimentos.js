8389997508f98eabb128c9be2656d36d368b4c13
iagolopess <iagoalopes@gmail.com>
2021-10-27 00:48:52 -0300

    Conexao com BD

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,5 @@
 // Controla as rotas
-
+const Atendimento = require("../models/atendimentos");
 module.exports = (app) => {
   //req = o que enviamos
   //res = o que recebemos
@@ -8,7 +8,8 @@ module.exports = (app) => {
   ); // Puxa dados para o navegador
 
   app.post("/atendimentos", (req, res) => {
-    console.log(req.body);
-    res.send("Você está na rota de atendimentos e está realizando um POST");
+    const atendimento = req.body;
+
+    Atendimento.adiciona(atendimento, res);
   }); // recebe dados do navegador
 };
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Esse arquivo sobe o servidor no ar
-
 const customExpress = require("./config/customExpress");
+const conexao = require("./infraestrutura/conexao");
+const tabelas = require("./infraestrutura/tabelas");
 
-const app = customExpress();
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log("conectado com sucesso");
+    tabelas.init(conexao);
+    const app = customExpress();
+    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+  }
+});

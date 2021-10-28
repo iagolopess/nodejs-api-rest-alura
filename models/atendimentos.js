31132f3fd0532601ebf2a7989f609c2b4e14750f
iagolopess <iagoalopes@gmail.com>
2021-10-28 00:59:41 -0300

    finalizado o POST

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,16 +7,40 @@ class Atendimento {
     const data = moment(atendimento.data, "DD/MM/YYYY").format(
       "YYYY-MM-DD HH:MM:SS"
     );
-    const atendimentoDatado = { ...atendimento, dataCriacao, data };
-    const sql = "INSERT INTO Atendimentos SET ?";
 
-    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-      if (erro) {
-        res.status(400).json(erro);
-      } else {
-        res.status(201).json(resultados);
-      }
-    });
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 4;
+
+    const validacoes = [
+      {
+        nome: "data",
+        valido: dataEhValida,
+        mensagem: "Data deve ser maior ou igual a data atual",
+      },
+      {
+        nome: "cliente",
+        valido: clienteEhValido,
+        mensagem: "Cliente deve ter pelo menos quatro caracteres",
+      },
+    ];
+
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
+
+    if (existemErros) {
+      res.status(400).json(erros);
+    } else {
+      const atendimentoDatado = { ...atendimento, dataCriacao, data };
+      const sql = "INSERT INTO Atendimentos SET ?";
+
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if (erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(resultados);
+        }
+      });
+    }
   }
 }
 module.exports = new Atendimento();
